912c34f72fdaaa67721b66c05753daebd6ad0572
sunilsGubbala <154649760+sunilsGubbala@users.noreply.github.com>
2023-12-23 12:38:08 +0530

    new files

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+
+function init(){
+const hamburger = document.querySelector(".hamburger");
+const sidebarCloseButton =document.querySelector(".close-button");
+const sidebarContainer = document.querySelector(".sidebar-container");
+
+const openSidebar = () => {
+    sidebarContainer .classList.add("show-sidebar");
+};
+const closeSidebar = () => {
+    sidebarContainer .classList.remove("show-sidebar");
+};
+
+hamburger.addEventListener("click",openSidebar);
+sidebarCloseButton.addEventListener("click",closeSidebar);
+}
+init();
+
+function navRelated(){
+    const navbarContainer=document.querySelector(".nav-container");
+    window.addEventListener("scroll",function(){
+        const scrollNumber = window.scrollY;
+        const targetNumber = window.innerWidth>=992 ? 70:50;
+        if (scrollNumber >= targetNumber){
+            navbarContainer.classList.add("sticky-nav");
+        } else{
+            navbarContainer.classList.remove("sticky-nav");
+        }
+    });
+}
+navRelated();
